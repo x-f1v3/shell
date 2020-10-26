efae1a4ffdf9482b5aabb848e5b7877203698fe8
x-f1v3 <xywwhn@163.com>
2020-10-26 12:36:26 +0800

    Update list_files.sh

--- list_files.sh
+++ list_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#
 
-function ergodic(){
+
+function ergodic(){ 
   for file in `ls $1`
   do
     if [ -d $1"/"$file ]
@@ -10,10 +10,32 @@ function ergodic(){
     else
       local path=$1"/"$file 
       local name=$file      
-      echo $name $path 
+      if [[ $name =~ $suffix ]]; then
+        # echo $name 
+          check=(`ls $target | grep $name | wc -l`) # 有重名文件替换
+          check=${check// /} # 清除空格
+         
+        if [[ $check -ne 0 ]]; then
+          cp $path $target$name'_'$RANDOM$suffix # $RANDOM 内置随机数
+        else
+          cp $path $target
+        fi
+        
+
+      fi
     fi
-  done
+ done
 }
+
 IFS=$'\n' #这个必须要，否则会在文件名中有空格时出错
 
-ergodic '/tmp/'
+
+suffix=".jar"
+source=""
+target=""
+
+ergodic $source 
+
+
+
+
